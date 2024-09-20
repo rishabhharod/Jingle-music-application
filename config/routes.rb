30b2a2a7ce3e2672e6d318906bd378e368be385a
Rishabh harod <rishabhharod4449@gmail.com>
2024-09-20 23:42:16 +0530

    added the routes of models

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   resource :song, only: [:create,:destroy]
-  resource :artist, only: [:create,:update,:destroy]
-  resource :collection, only: [:create,:update,:destroy]
+  resource :artist, only: [:create,:update,:destroy,:show]
   resource :playlist, only: [:create,:destroy]
+  resource :user_playlist, only: [:create,:destroy]
+  resources :wishlists, only: [:create,:index]
+  resources :collection
 
   post 'login', to: 'authentication#login'
 
